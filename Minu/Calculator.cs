3e9418ed5fb32e615bb8c0f7594a72537756cedb
HITGIF <carbonylgp@gmail.com>
2018-11-17 18:26:48 +0800

    [Fix] Elegant line calculation

--- Minu/Calculator.cs
+++ Minu/Calculator.cs
@@ -11,79 +11,66 @@ namespace Minu {
 
         private static Regex functionRegex = new Regex(@"\(.*?\)\s*=");
 
-        public string Calculate(string rawInput) {
-            if (characterPerLine < 0) return "";
+        public List<string> Calculate(string rawInput) {
+            if (characterPerLine < 0) return new List<string>();
 
             IOutputFormatter outputFormatter = new DecFormatter();
 
             isInputOverflowed = false;
-
-            string outputText = "";
+            
             string[] inputs = rawInput.Replace("\r", "").Split('\n');
-
+            
+            var resultList = new List<string>();
             var arguments = new List<Argument>();
             var functions = new List<Function>();
             double ans = 0.0;
-            int count = 0;
 
-            foreach (string input in inputs) {
+            foreach (string inputLine in inputs) {
 
-                count++;
-
-                // Add \n to align with the input
-                var inputActualLine = Math.Max(1, (int)Math.Ceiling((double)input.Length / characterPerLine));
-                if (inputActualLine > 1)
-                    isInputOverflowed = true;
-                //if (count > 1)
-                    outputText += new string('\n', inputActualLine - 1);
-
-                if (input.StartsWith("#")) { // comments + settings
-                    string trimed = input.Substring(1).TrimStart().ToLower();
+                if (inputLine.StartsWith("#")) { // comments + settings
+                    string trimed = inputLine.Substring(1).TrimStart().ToLower();
                     if (trimed.StartsWith("bin")) outputFormatter = new BinFormatter();
                     else if (trimed.StartsWith("oct")) outputFormatter = new OctFormatter();
                     else if (trimed.StartsWith("dec")) outputFormatter = new DecFormatter();
                     else if (trimed.StartsWith("hex")) outputFormatter = new HexFormatter();
                     else if (trimed.StartsWith("sci")) outputFormatter = new SciFormatter();
                 }
-                else if (functionRegex.IsMatch(input)) { // functions
+                else if (functionRegex.IsMatch(inputLine)) { // functions
                     bool overrided = false;
-                    Function func = new Function(input);
+                    Function func = new Function(inputLine);
                     func.addFunctions(functions.ToArray());
                     if (func.getFunctionName() != null) {
                         if (functions.RemoveAll(f => f.getFunctionName() == func.getFunctionName()) > 0) // override occurred
                             overrided = true;
                         functions.Add(func);
-
-                        //show function assignment
+                        resultList.Add("");
                         //outputText += (overrided ? "(*) " : "") + func.getFunctionName();
                     }
                 }
-                else if (input.Contains("=")) { // variables
+                else if (inputLine.Contains("=")) { // variables
                     bool overrided = false;
-                    Argument arg = new Argument(input, new Argument("ans", ans));
+                    Argument arg = new Argument(inputLine, new Argument("ans", ans));
                     arg.addArguments(arguments.ToArray());
                     arg.addFunctions(functions.ToArray());
                     if (arguments.RemoveAll(a => a.getArgumentName() == arg.getArgumentName()) > 0) // override occurred
                         overrided = true;
                     arguments.Add(arg);
-                    outputText +=
-                        //show variable assignment
-                        //(overrided ? "(*) " : "") + arg.getArgumentName() + " = " +
-                        outputFormatter.Format(arg.getArgumentValue());
+                    //outputText += (overrided ? "(*) " : "") + arg.getArgumentName() + " = " +
+                    //outputFormatter.Format(arg.getArgumentValue());
+                    resultList.Add(arg.getArgumentValue().ToString());
                 }
-                else if (input != "") { // evaluate
-                    var expression = new Expression(input, new Argument("ans", ans));
+                else if (inputLine != "") { // evaluate
+                    var expression = new Expression(inputLine, new Argument("ans", ans));
                     expression.addArguments(arguments.ToArray());
                     expression.addFunctions(functions.ToArray());
                     var result = expression.calculate();
                     if (!double.IsNaN(result)) {
                         ans = result;
-                        outputText += outputFormatter.Format(result);
+                        resultList.Add(outputFormatter.Format(result));
                     }
                 }
-                outputText += '\n';
             }
-            return outputText.TrimEnd('\n');
+            return resultList;
         }
     }
 }
--- Minu/MainWindow.xaml.cs
+++ Minu/MainWindow.xaml.cs
@@ -7,6 +7,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using ICSharpCode.AvalonEdit;
 using ICSharpCode.AvalonEdit.Highlighting;
+using ICSharpCode.AvalonEdit.Rendering;
 using org.mariuszgromada.math.mxparser;
 using Expression = org.mariuszgromada.math.mxparser.Expression;
 
@@ -23,9 +24,48 @@ namespace Minu {
             // Debug
             input.Text = "sqrare_root(x)=sqrt(x)\nva = sqrare_root(2*6.21*3.77*10^5) \npr = .01*2*va\nvnew = pr/2\nta = va/6.21\ng = 9.8\nvland = tan(vnew^2+2*9.8*1.6*10^4)\nF = 2*vland/.5\ntl = (vland - vnew)/g \nF/.15*100000\ntpfff = (−.5*vland+ sqrare_root((0.5*vland)^2−4*. 5*g*−35))/2*.5*g\ntp = sin(35/(.5*g)) \n.5*vland*tp \nta+tl+tp+30+.5\nva\nvnew";
         }
-        
+
         private void recalculate() {
-            output.Text = calculator.Calculate(input.Text);
+
+            #region Calculate visual line count for every input line after wrapping
+            // Write a temporary line in output for estimating base line height, deleted afterwards anyway
+            // WARNING: assumming output & input editors have the same line height
+            output.Text = "\n";
+            double baseLineHeight = output.TextArea.TextView.DocumentHeight - output.TextArea.TextView.GetVisualTopByDocumentLine(output.LineCount);
+
+            // Contains the number of lines visually presented in each input line after wrapping
+            var visualLineNum = new List<int>();
+            for (int i = 0; i < input.LineCount; i++) {
+                double bottom = 0;
+                double top = input.TextArea.TextView.GetVisualTopByDocumentLine(i + 1);
+                if (i + 2 <= input.LineCount)
+                    bottom = input.TextArea.TextView.GetVisualTopByDocumentLine(i + 2);
+                else
+                    bottom = input.TextArea.TextView.DocumentHeight;
+                visualLineNum.Add((int)Math.Round((bottom - top) / baseLineHeight));
+                Console.WriteLine((i + 1).ToString() + '\t' + visualLineNum[i]);
+            }
+            #endregion
+
+            #region Write output
+            output.Text = "";
+            var resultList = calculator.Calculate(input.Text);
+            int count = -1;
+            foreach (string line in resultList) {
+                count++;
+                // Line up to input
+                if (count == 0) {
+                    // One less '\n' on the first line
+                    for (int i = 0; i < visualLineNum[count] - 1; i++)
+                        output.Text += '\n';
+                    output.Text += line;
+                    continue;
+                }
+                for (int i = 0; i < visualLineNum[count]; i++)
+                    output.Text += '\n';
+                output.Text += line;
+            }
+            #endregion
 
             // Show the splitter if necessary
             bool outputOverflowed = (outputColumn.ActualWidth - output.ActualWidth - output.Margin.Left - output.Margin.Right) <= 10;
