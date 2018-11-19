db8fa49caadd1b8c64f56808c098e43050d4396e
Harry Null <harryyunull@gmail.com>
2018-11-19 19:25:09 +0800

    Add cache to selection

--- Minu/MainWindow.xaml.cs
+++ Minu/MainWindow.xaml.cs
@@ -18,6 +18,9 @@ namespace Minu {
     public partial class MainWindow : Window {
 
         Calculator calculator = new Calculator();
+        Dictionary<int, VisualLineInfo> visualLineInfoCache = new Dictionary<int, VisualLineInfo>();
+        int lastHighlightedLine = -1;
+
         double baseLineHeight;
 
         public MainWindow() {
@@ -37,23 +40,44 @@ namespace Minu {
         }
 
         private void output_MouseMove(object sender, EventArgs e) {
-            if (output.LineCount <= 0) return;
-            output.Text = output.Text.Replace("\u2000", "");
 
+            if (output.LineCount <= 0) return;
+
+            // Estimate the line number according to cursor position
             Point p = Mouse.GetPosition(output);
             int lineNum = (int)Math.Ceiling(p.Y / baseLineHeight);
-            if (lineNum > output.LineCount) return;
 
-            var line = output.TextArea.Document.GetLineByNumber(lineNum);
-            var visualLine = output.TextArea.TextView.GetOrConstructVisualLine(line);
-            if (visualLine == null) return; // ignore invisible lines
+            if (lineNum <= 0 || lineNum > output.LineCount) {
+                if (lastHighlightedLine != -1) {
+                    output.TextArea.Document.Remove(visualLineInfoCache[lastHighlightedLine].Offset, 2);
+                    lastHighlightedLine = -1;
+                }
+                return;
+            }
 
-            var visualXStart = visualLine.GetVisualPosition(0, VisualYPosition.Baseline).X;
-            var visualXEnd = visualLine.GetVisualPosition(line.Length, VisualYPosition.Baseline).X;
-            if (p.X < visualXStart || p.X > visualXEnd) return;
+            // Rebuild cache if hasn't yet
+            if (visualLineInfoCache.Count == 0) {
+                for (int i = 1; i <= output.TextArea.Document.LineCount; ++i)
+                    visualLineInfoCache.Add(i, new VisualLineInfo(output, i));
+            }
 
-            if (line.ToString().Contains("\u2000")) return;
-            output.TextArea.Document.Insert(line.Offset, "\u2000\u2000");
+            // Calculated if the line is actually selected.
+            var visualLine = visualLineInfoCache[lineNum];
+            bool validSelection = visualLine.Length != 0 && visualLine.XStart <= p.X && p.X <= visualLine.XEnd;
+
+            // If (1) anything is highlighted and (2) the current line is not to be highlighted,
+            // remove the old highlight mark
+            if (lastHighlightedLine != -1 && (lastHighlightedLine != lineNum || !validSelection)) {
+                output.TextArea.Document.Remove(visualLineInfoCache[lastHighlightedLine].Offset, 2);
+                lastHighlightedLine = -1;
+            }
+
+            if (lineNum == lastHighlightedLine) return; // if already selected, do nothing
+            if (!validSelection) return; // if nothing is selected, do nothing
+            
+            output.TextArea.Document.Insert(visualLine.Offset, "\u2000\u2000");
+
+            lastHighlightedLine = lineNum;
         }
 
         private void recalculate() {
@@ -73,9 +97,10 @@ namespace Minu {
                 outputText += (bound > 0?(new string('\n', bound)):"") + resultList[i] + "\u2002\u2001";
             }
             output.Text = outputText;
-            output.InvalidateMeasure();
 
-            output.InvalidateVisual();
+            visualLineInfoCache.Clear();
+            lastHighlightedLine = -1;
+
             // Show the splitter if necessary
             bool outputOverflowed = (outputColumn.ActualWidth - output.ActualWidth - output.Margin.Left - output.Margin.Right) <= 10;
             if (outputOverflowed || calculator.isInputOverflowed)
